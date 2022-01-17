7e0c6f48b8c52d44d54777be34662c6228fcaca5
OlhaNester <olga_nv@ukr.net>
2022-01-17 17:17:26 +0200

    Formik

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Field, Formik, ErrorMessage } from 'formik';
-import { nanoid } from 'nanoid';
+
 import * as yup from 'yup';
 
 
 import { Form, Label, Button, ErrorText } from './ContactForm.styled';
 
 const schema = yup.object().shape({
-  name: yup.string().min(1).max(20).required(),
-  number: yup.string().min(8).max(9).required(),
+  name: yup.string().matches(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/, `Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan`,).required('Please enter your name, it is required'),
+  number: yup.string().matches(/\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/, `Phone number must be digits and can contain spaces, dashes, parentheses and can start with +`,).required('Please enter your phone number, it is required'),
 })
 
 const FormError = ({ name }) => {
@@ -25,8 +25,7 @@ export class CotactForm extends Component {
     name: '',
     number: '',
   };
-  nameInputId = nanoid();
-  numberInputId = nanoid();
+ 
 
   handleChange = event => {
     const { name, value } = event.currentTarget;
@@ -56,12 +55,7 @@ export class CotactForm extends Component {
               id={this.nameInputId}
               value={this.state.name}
               onChange={this.handleChange}
-              type="text"
-                name="name"
-               
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
+             
               />
               </Label>
             <FormError name="name" />
@@ -74,12 +68,7 @@ export class CotactForm extends Component {
               id={this.numberInputId}
               value={this.state.number}
               onChange={this.handleChange}
-              type="tel"
-                name="number"
-               
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
+              
               />
               
             </Label>
